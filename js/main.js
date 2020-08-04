ab6a791392e0d101c3a13ef5d33405117b58df97
Roman <rom.nosov2014@yandex.ru>
2020-08-04 14:17:11 +0300

    new files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        //el: '.project-pagination',
+        el: '.project-pagination',
+        bulletClass: 'project-bullet',
+        bulletActiveClass: 'project-bullet-active',
+        clickable: true 
+
+      }
+});
